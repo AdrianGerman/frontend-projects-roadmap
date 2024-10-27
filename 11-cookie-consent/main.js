1826928d5ac04e9c710582afcc1ef02303f032b4
AdrianGerman <adrian.german1019@gmail.com>
2024-10-27 10:16:54 -0600

    disable scroll in cookie window

--- 11-cookie-consent/main.js
+++ 11-cookie-consent/main.js
@@ -4,13 +4,25 @@ document.addEventListener("DOMContentLoaded", () => {
   const acceptButton = document.querySelector(".button-cookie button")
   const closeButton = document.querySelector(".close-icon")
 
+  // desactivar la pagina hasta que se cierre la ventana de cookies
+  function disablePage() {
+    document.body.style.overflow = "hidden"
+  }
+
+  // función para habilitar la pagina
+  function enablePage() {
+    document.body.style.overflow = ""
+  }
+
   // verifica si ya se aceptaron las cookies anteriormente
   if (localStorage.getItem("cookiesAccepted") == "true") {
     cookieMenu.style.display = "none"
     overlay.style.display = "none"
+    enablePage()
   } else {
     cookieMenu.style.display = "flex"
     overlay.style.display = "block"
+    disablePage()
   }
 
   // boton de aceptar cookies
@@ -18,11 +30,13 @@ document.addEventListener("DOMContentLoaded", () => {
     localStorage.setItem("cookiesAccepted", "true")
     cookieMenu.style.display = "none"
     overlay.style.display = "none"
+    enablePage()
   })
 
   // boton cerrar
   closeButton.addEventListener("click", () => {
     cookieMenu.style.display = "none"
     overlay.style.display = "none"
+    enablePage()
   })
 })
