f5214c5b11a2c0c03c6a4e35d869dc131ff3f6e2
AdrianGerman <adrian.german1019@gmail.com>
2024-10-28 10:02:10 -0600

    logic js

--- 12-restricted-textarea/main.js
+++ 12-restricted-textarea/main.js
@@ -1,1 +1,16 @@
-console.log("Hola, desde el js")
+const messageElement = document.getElementById("message")
+const charCountElement = document.getElementById("charCount")
+const maxChars = 250
+
+messageElement.addEventListener("input", () => {
+  const currentLength = messageElement.value.length
+  charCountElement.textContent = `${currentLength} / ${maxChars}`
+
+  if (currentLength >= maxChars) {
+    messageElement.classList.add("char-limit")
+    charCountElement.classList.add("char-limit")
+  } else {
+    messageElement.classList.remove("char-limit")
+    charCountElement.classList.remove("char-limit")
+  }
+})
