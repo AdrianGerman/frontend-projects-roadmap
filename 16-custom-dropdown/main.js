4d0d5ae31b4e8d6ef7865471a7104891aabcad6c
AdrianGerman <adrian.german1019@gmail.com>
2024-11-24 10:50:20 -0600

    js logic for item selector

--- 16-custom-dropdown/main.js
+++ 16-custom-dropdown/main.js
@@ -1,1 +1,31 @@
-console.log("Hello, from the js")
+const dropdownHeader = document.getElementById("dropdownHeader")
+const dropdownOptions = document.getElementById("dropdownOptions")
+const options = document.querySelectorAll(".dropdown-option")
+
+dropdownHeader.addEventListener("click", () => {
+  dropdownOptions.classList.toggle("open")
+  dropdownHeader.parentElement.classList.toggle("open")
+})
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    const chevron = dropdownHeader.querySelector(".chevron")
+    dropdownHeader.innerHTML = `${option.textContent} <span class="chevron">${chevron.textContent}</span>`
+
+    dropdownOptions.classList.remove("open")
+    dropdownHeader.parentElement.classList.remove("open")
+
+    options.forEach((opt) => opt.classList.remove("selected"))
+    option.classList.add("selected")
+  })
+})
+
+document.addEventListener("click", (e) => {
+  if (
+    !dropdownHeader.contains(e.target) &&
+    !dropdownOptions.contains(e.target)
+  ) {
+    dropdownOptions.classList.remove("open")
+    dropdownHeader.parentElement.classList.remove("open")
+  }
+})
