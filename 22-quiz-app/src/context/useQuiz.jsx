c7e6584346ce49b8a77f20e5e079040a516b3d03
AdrianGerman <adrian.german1019@gmail.com>
2024-12-04 11:28:03 -0600

    install zustand and global state

---
+++ 22-quiz-app/src/context/useQuiz.jsx
@@ -0,0 +1,19 @@
+import create from "zustand"
+
+const useQuiz = create((set) => ({
+  score: 0,
+  currentQuestionIndex: 0,
+  timeLeft: 60,
+  incrementScore: () => set((state) => ({ score: state.score + 1 })),
+  nextQuestion: () =>
+    set((state) => ({
+      currentQuestionIndex: state.currentQuestionIndex + 1,
+      timeLeft: 60
+    })),
+  resetQuiz: () =>
+    set((state) => ({ timeLeft: state.timeLeft > 0 ? state.timeLeft - 1 : 0 })),
+  decrementTime: () =>
+    set((state) => ({ timeLeft: state.timeLeft > 0 ? state.timeLeft - 1 : 0 }))
+}))
+
+export default useQuiz
