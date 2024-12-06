b45a0cbf8820a155967e7b2ea00d102301303413
AdrianGerman <adrian.german1019@gmail.com>
2024-12-06 13:43:05 -0600

    Adding weather search and loading - error

--- 23-weather-web-app/src/App.jsx
+++ 23-weather-web-app/src/App.jsx
@@ -1,10 +1,42 @@
+import { useState } from "react"
+import { motion } from "framer-motion"
+import { fetchWeather } from "./utils/api"
+import WeatherSearch from "./components/WeatherSearch"
 import "./App.css"
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
+
+  const handleSearch = async (location) => {
+    setLoading(true)
+    setError("")
+    try {
+      const data = await fetchWeather(location)
+      setWeatherData(data)
+    } catch (error) {
+      setError(
+        "Error al obtener los datos del clima. Por favor, intente de nuevo."
+      )
+    }
+    setLoading(false)
+  }
   return (
-    <>
-      <h1 className="font-bold text-4xl">Aplicación del clima</h1>
-    </>
+    <div className="text-center text-white">
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="bg-white/10 backdrop-blur-md rounded-2xl p-8 shadow-md max-w-md w-full"
+      >
+        <h1 className="font-bold text-xl mb-6">Aplicación del clima</h1>
+        <WeatherSearch onSearch={handleSearch} />
+
+        {loading && <p className="">Cargando...</p>}
+        {error && <p className="">{error}</p>}
+      </motion.div>
+    </div>
   )
 }
 
