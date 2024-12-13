42c6c81dbef2b7c8046a225be972f5dc7d9fd158
AdrianGerman <adrian.german1019@gmail.com>
2024-12-13 12:40:24 -0600

    sample projects for content layout

---
+++ web/src/data/projects.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    image: "public/projects/01.webp",
+    title: "Single page CV",
+    description:
+      "Crea un CV HTML de una sola página para mostrar tu historial profesional",
+    tags: ["HTML", "Semantic HTML", "Layout", "SEO"],
+    repo: "https://github.com/AdrianGerman/frontend-projects-roadmap/tree/main/01-single-page-CV",
+    demo: "/01-single-page-CV/index.html",
+    details: "https://roadmap.sh/projects/single-page-cv"
+  },
+  {
+    id: 2,
+    image: "public/projects/02.webp",
+    title: "Basic HTML Viewer",
+    description: "Cree un sitio web simple, solo en HTML, con varias páginas",
+    tags: ["HTML", "Semantic HTML", "Layouts"],
+    repo: "https://github.com/AdrianGerman/frontend-projects-roadmap/tree/main/02-basic-HTML-website",
+    demo: "/02-basic-HTML-website/index.html",
+    details: "https://roadmap.sh/projects/basic-html-website"
+  }
+]
