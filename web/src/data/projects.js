c70e50e86a054dcb8406238d8536d1d34128bd31
AdrianGerman <adrian.german1019@gmail.com>
2024-12-15 12:12:03 -0600

    projects info

--- web/src/data/projects.js
+++ web/src/data/projects.js
@@ -1,10 +1,10 @@
 export default [
   {
     id: 1,
-    image: "public/projects/01.webp",
+    image: "/projects/01.webp",
     title: "Single page CV",
     description:
-      "Crea un CV HTML de una sola página para mostrar tu historial profesional",
+      "Crea un CV HTML de una sola página para mostrar tu historial profesional.",
     tags: ["HTML", "Semantic HTML", "Layout", "SEO"],
     repo: "https://github.com/AdrianGerman/frontend-projects-roadmap/tree/main/01-single-page-CV",
     demo: "/01-single-page-CV/index.html",
@@ -12,12 +12,45 @@ export default [
   },
   {
     id: 2,
-    image: "public/projects/02.webp",
+    image: "/projects/02.webp",
     title: "Basic HTML Viewer",
-    description: "Cree un sitio web simple, solo en HTML, con varias páginas",
+    description: "Cree un sitio web simple, solo en HTML, con varias páginas.",
     tags: ["HTML", "Semantic HTML", "Layouts"],
     repo: "https://github.com/AdrianGerman/frontend-projects-roadmap/tree/main/02-basic-HTML-website",
     demo: "/02-basic-HTML-website/index.html",
     details: "https://roadmap.sh/projects/basic-html-website"
+  },
+  {
+    id: 3,
+    image: "/projects/03.webp",
+    title: "Personal Portfolio",
+    description:
+      "Convierte un sitio web HTML simple en un portafolio personal.",
+    tags: ["CSS", "Responsive Design", "Box Model", "Flexbox", "Typography"],
+    repo: "https://github.com/AdrianGerman/frontend-projects-roadmap/tree/main/03-personal-portfolio",
+    demo: "/03-personal-portfolio/index.html",
+    details: "https://roadmap.sh/projects/portfolio-website"
+  },
+  {
+    id: 4,
+    image: "/projects/04.webp",
+    title: "Changelog Component",
+    description:
+      "Crear un componente de registro de cambios para un sitio web utilizando HTML y CSS.",
+    tags: ["HTML", "Layouts", "CSS", "Flexbox"],
+    repo: "https://github.com/AdrianGerman/frontend-projects-roadmap/tree/main/04-changelog-component",
+    demo: "/04-changelog-component/index.html",
+    details: "https://roadmap.sh/projects/changelog-component"
+  },
+  {
+    id: 5,
+    image: "/projects/05.webp",
+    title: "Testimonial Cards",
+    description:
+      "Crear tarjetas de recomendación para un sitio web utilizando HTML y CSS.",
+    tags: ["HTML", "Layouts", "CSS", "Flexbox", "Positioning"],
+    repo: "https://github.com/AdrianGerman/frontend-projects-roadmap/tree/main/05-testimonial-cards",
+    demo: "/05-testimonial-cards/index.htmll",
+    details: "https://roadmap.sh/projects/testimonial-cards"
   }
 ]
