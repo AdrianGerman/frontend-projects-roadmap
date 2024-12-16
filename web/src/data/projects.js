f00e252aab3332073b489fb814071c820add1f5c
AdrianGerman <adrian.german1019@gmail.com>
2024-12-16 08:28:43 -0600

    change in the path of the images and add projects

--- web/src/data/projects.js
+++ web/src/data/projects.js
@@ -1,67 +1,67 @@
 export default [
   {
     id: 1,
-    image: "/projects/01.webp",
+    image: "/images/01.webp",
     title: "Single page CV",
     description:
       "Crea un CV HTML de una sola página para mostrar tu historial profesional.",
     tags: ["HTML", "Semantic HTML", "Layout", "SEO"],
     repo: "https://github.com/AdrianGerman/frontend-projects-roadmap/tree/main/01-single-page-CV",
-    demo: "/01-single-page-CV/index.html",
+    demo: "/projects/01-single-page-CV/index.html",
     details: "https://roadmap.sh/projects/single-page-cv"
   },
   {
     id: 2,
-    image: "/projects/02.webp",
+    image: "/images/02.webp",
     title: "Basic HTML Viewer",
     description: "Cree un sitio web simple, solo en HTML, con varias páginas.",
     tags: ["HTML", "Semantic HTML", "Layouts"],
     repo: "https://github.com/AdrianGerman/frontend-projects-roadmap/tree/main/02-basic-HTML-website",
-    demo: "/02-basic-HTML-website/index.html",
+    demo: "/projects/02-basic-HTML-website/index.html",
     details: "https://roadmap.sh/projects/basic-html-website"
   },
   {
     id: 3,
-    image: "/projects/03.webp",
+    image: "/images/03.webp",
     title: "Personal Portfolio",
     description:
       "Convierte un sitio web HTML simple en un portafolio personal.",
     tags: ["CSS", "Responsive Design", "Box Model", "Flexbox", "Typography"],
     repo: "https://github.com/AdrianGerman/frontend-projects-roadmap/tree/main/03-personal-portfolio",
-    demo: "/03-personal-portfolio/index.html",
+    demo: "/projects/03-personal-portfolio/index.html",
     details: "https://roadmap.sh/projects/portfolio-website"
   },
   {
     id: 4,
-    image: "/projects/04.webp",
+    image: "/images/04.webp",
     title: "Changelog Component",
     description:
       "Crear un componente de registro de cambios para un sitio web utilizando HTML y CSS.",
     tags: ["HTML", "Layouts", "CSS", "Flexbox"],
     repo: "https://github.com/AdrianGerman/frontend-projects-roadmap/tree/main/04-changelog-component",
-    demo: "/04-changelog-component/index.html",
+    demo: "/projects/04-changelog-component/index.html",
     details: "https://roadmap.sh/projects/changelog-component"
   },
   {
     id: 5,
-    image: "/projects/05.webp",
+    image: "/images/05.webp",
     title: "Testimonial Cards",
     description:
       "Crear tarjetas de recomendación para un sitio web utilizando HTML y CSS.",
     tags: ["HTML", "Layouts", "CSS", "Flexbox", "Positioning"],
     repo: "https://github.com/AdrianGerman/frontend-projects-roadmap/tree/main/05-testimonial-cards",
-    demo: "/05-testimonial-cards/index.html",
+    demo: "/projects/05-testimonial-cards/index.html",
     details: "https://roadmap.sh/projects/testimonial-cards"
   },
   {
     id: 6,
-    image: "/projects/06.webp",
+    image: "/images/06.webp",
     title: "Datepicker UI",
     description:
       "Crear una interfaz de usuario de selector de fechas simple utilizando HTML y CSS.",
     tags: ["HTML", "Layouts", "CSS", "Positioning"],
     repo: "https://github.com/AdrianGerman/frontend-projects-roadmap/tree/main/06-datepicker-ui",
-    demo: "/06-datepicker-ui/index.html",
+    demo: "/projects/06-datepicker-ui/index.html",
     details: "https://roadmap.sh/projects/datepicker-ui"
   }
 ]
